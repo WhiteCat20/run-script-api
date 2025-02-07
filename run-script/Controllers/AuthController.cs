a4d525d01eaadfbe106da28b3d4333ba21519f94
WhiteCat20 <fzrahmadan@gmail.com>
2025-02-07 10:49:03 +0700

    creating register and login method

---
+++ run-script/Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using run_script.Models;
+
+namespace run_script.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+        // post /api/auth/register
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO request)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = request.Username,
+                Email = request.Username
+            };
+            var identityResult = await userManager.CreateAsync(identityUser, request.Password);
+            // check if its succeed
+            if (identityResult.Succeeded)
+            {
+                // add role to this user
+                if (request.Roles != null && request.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, request.Roles);
+                    if (identityResult.Succeeded)
+                    {
+                        return Ok("User was registered! Now Login!");
+                    }
+                }
+            }
+
+
+            return BadRequest("Something went wrong!");
+
+        }
+
+        // POST : /api/Auth/Login
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginRequestDto)
+        {
+            var user = await userManager.FindByEmailAsync(loginRequestDto.Username);
+            if (user != null)
+            {
+                var checkPasswordResult = await userManager.CheckPasswordAsync(user, loginRequestDto.Password);
+                if (checkPasswordResult)
+                {
+                    // create token 
+
+                    return Ok();
+                }
+                return BadRequest("Password Incorect!");
+            }
+            return BadRequest("Username or Password Incorect!");
+        }
+    }
+}
---
+++ run-script/Models/RegisterDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace run_script.Models
+{
+    public class RegisterDTO
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string[] Roles { get; set; }
+
+    }
+}
